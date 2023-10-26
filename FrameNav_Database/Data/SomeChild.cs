3c20aa7bc659c0526324c0001cd02bf234f70d9b
shaunstummer <7850615+shaunstummer@users.noreply.github.com>
2023-10-26 11:38:25 +1100

    Update

---
+++ FrameNav_Database/Data/SomeChild.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameNav_Database.Data
+{
+    internal class SomeChild:Repository
+    {
+        public void CloneRepo()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void PrintRepoChild()
+        {
+
+        }
+    }
+}
